a2841f1c3edd08e331a23697ac3b46697ad1f8b9
uuulis <listratovaul@gmail.com>
2026-04-20 13:02:15 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,39 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    T data[size];
+    int top;
+
+ public:
+    TStack() : top(-1) {}
+
+    void Push(const T& value) {
+        if (top < size - 1) {
+            data[++top] = value;
+        }
+    }
+
+    void Pop() {
+        if (top >= 0) {
+            --top;
+        }
+    }
+
+    T Top() const {
+        if (top >= 0) {
+            return data[top];
+        }
+        return T();
+    }
+
+    bool IsEmpty() const {
+        return top == -1;
+    }
+
+    bool IsFull() const {
+        return top == size - 1;
+    }
 };
 
-#endif  // INCLUDE_TSTACK_H_
+#endif // INCLUDE_TSTACK_H_
